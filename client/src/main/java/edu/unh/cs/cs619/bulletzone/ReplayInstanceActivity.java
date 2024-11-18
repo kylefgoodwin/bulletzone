1c9c47373a4600bc92e3026edc2714c1aaacfc2a
Alec <arydeen@comcast.net>
2024-11-18 11:40:43 -0500

    JavaDoc

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/ReplayInstanceActivity.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/ReplayInstanceActivity.java
@@ -24,6 +24,13 @@ import edu.unh.cs.cs619.bulletzone.events.ReplayEventProcessor;
 import edu.unh.cs.cs619.bulletzone.rest.GridReplayTask;
 import edu.unh.cs.cs619.bulletzone.util.ReplayData;
 
+/**
+ * Made by Alec Rydeen
+ *
+ * Activity to act as the actual replay instance, where the replay is shown
+ * Has options to play and pause the replay, as well as to adjust
+ * the speed at which the replay is shown
+ */
 @EActivity(R.layout.activity_replay_instance)
 public class ReplayInstanceActivity extends Activity {
 
