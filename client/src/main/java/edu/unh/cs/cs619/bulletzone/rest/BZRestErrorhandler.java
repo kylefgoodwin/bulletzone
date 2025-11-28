df19a8aa6164a307f8b8cffd5e9e845aaf2ca6fe
kylefgoodwin <kgoodwin109@gmail.com>
2025-11-28 16:55:44 -0500

    Clean up BZRestErrorhandler by removing comment
    Removed unnecessary comment block from BZRestErrorhandler.

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BZRestErrorhandler.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BZRestErrorhandler.java
@@ -4,9 +4,6 @@ import org.androidannotations.annotations.EBean;
 import org.androidannotations.rest.spring.api.RestErrorHandler;
 import org.springframework.core.NestedRuntimeException;
 
-/**
- * Created by simon on 10/3/14.
- */
 @EBean
 public class BZRestErrorhandler implements RestErrorHandler {
 
